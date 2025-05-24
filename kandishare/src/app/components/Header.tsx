65aefbee529b16138e18bc1e9fca78c0d063e2ac
charliefd1999 <charliefd1999@gmail.com>
2025-05-23 17:05:06 -0700

    Fixing hydration issue

--- kandishare/src/app/components/Header.tsx
+++ kandishare/src/app/components/Header.tsx
@@ -1,10 +1,18 @@
 "use client";
 
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { getRandomizedColors } from "../utils/utils";
 
 export default function Header() {
+  const [hasHydrated, setHasHydrated] = useState(false);
+
+  useEffect(() => {
+    setHasHydrated(true);
+  }, []);
+
   const randomizedColors = useMemo(() => getRandomizedColors(), []);
+
+  if (!hasHydrated) return null;
 
   return (
     <div className="fixed top-0 left-0 w-full h-[10vh] flex bg-[#0A0A0A]">
